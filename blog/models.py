b7daf0edb2c75bc800da76b29d425588697d640d
CodeMaestro1000 <timothyakoji@gmail.com>
2021-11-09 23:20:02 +0100

    Project Proof-of-Concept

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.TextField()
+    author = models.CharField(max_length=100)
+    url = models.URLField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title[:50]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Project
 # Create your views here.
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class ProjectPageView(ListView):
+    template_name = 'projects.html'
+    model = Project
+    queryset = Project.objects.order_by('-date')
+    context_object_name = 'project_lists'
